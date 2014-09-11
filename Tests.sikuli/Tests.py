979a9f838506cfa93d56ada8bb026dc8d4c4d208
OtyuskiyRoman <otyuskiy@gmail.com>
2014-09-12 01:55:43 +0300

    Added my Sikuli and Selenium tests

---
+++ Tests.sikuli/Tests.py
@@ -0,0 +1,18 @@
+click(Pattern("1410475435395.png").similar(0.67))
+wait(Pattern("1410475724303.png").similar(0.40))
+click("1410475520665.png")
+wait(Pattern("1410193107382.png").similar(0.48), 5)
+type(Pattern("1410200199897.png").similar(0.91).targetOffset(-210,-10), "otyuskiy@gmail.com")
+type(Pattern("1410200557706.png").similar(0.88).targetOffset(239,37), 'Test')
+type(Pattern("1410192929725.png").similar(0.89).targetOffset(-3,56), 'Buenos nochas, Amigo!')
+click("1410194237883.png")
+
+
+        
+
+
+
+
+
+
+
