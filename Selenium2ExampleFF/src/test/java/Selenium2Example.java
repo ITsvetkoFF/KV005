313ef23ea7fda951b8fc86b99c697fe2a95e864f
unknown <vmoroztc@KVCLASS02.training.local>
2014-09-18 11:47:43 +0300

    HtmlUnitDriver

--- Selenium2ExampleFF/src/test/java/Selenium2Example.java
+++ Selenium2ExampleFF/src/test/java/Selenium2Example.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
