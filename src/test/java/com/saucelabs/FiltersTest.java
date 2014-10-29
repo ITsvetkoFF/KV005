a44ee28cae6f2ee327eb1c284a6ad02e6f7d79ef
shelcovitca <tinadiko223>
2014-10-29 21:34:07 +0200

    changes in FiltersTest

--- src/test/java/com/saucelabs/FiltersTest.java
+++ src/test/java/com/saucelabs/FiltersTest.java
@@ -3,6 +3,8 @@ package com.saucelabs;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,83 +13,58 @@ import java.util.concurrent.TimeUnit;
  * Created by nklimotc on 21.10.2014.
  */
 
-public class FiltersTest{
+public class FiltersTest {
+    public static int typeNumber;
+    WebDriver driver;
+    AnyPage anyPage;
+    MapPage mapPage;
+    ProblemPage problemPage;
+
+    @BeforeSuite
+    public void setUp() {
+        this.driver = new FirefoxDriver();
+        this.anyPage = new AnyPage(driver);
+        this.mapPage = new MapPage(driver);
+        this.problemPage = new ProblemPage(driver);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://127.0.0.1:8090/#/map");
+        driver.manage().window().maximize();
+    }
+
+    @AfterSuite
+    public void turnDown() {
+        this.driver.quit();
+    }
 
     @Test
-    public void checkFilters () throws Exception {
-
-        WebDriver driver = new FirefoxDriver();
-        AnyPage anyPage = new AnyPage(driver);
-        MapPage mapPage = new MapPage(driver);
-        ProblemPage problemPage = new ProblemPage(driver);
-
-
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-
-        driver.get("http://127.0.0.1:8090/#/map");
-
-        driver.manage().window().maximize();
-
-        //new AlertCloserThread().start();
-        //anyPage.addProblem(50.2, 30.2, "ProblemFor Проблеми лісів", "Проблеми лісів", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment1");
-
+    public void checkFiltersNegative() throws Exception {
         mapPage.clickZoomOut();
         mapPage.openFiltersBoard();
+        mapPage.selectAllExceptOneFilter(typeNumber);
+        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(typeNumber)));
+    }
 
-//        mapPage.selectAllExceptOneFilter(1);
-//
-//        try {
-//        mapPage.clickAtProblemByCoordinate(50.2, 30.2);
-//        } catch (Exception e) {
-//            e.getStackTrace();
-//        }
-//
-//        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(1)));
-
-//        anyPage.addProblem(50.3, 30.3, "ProblemFor Сміттєзвалища", "Сміттєзвалища", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment2");
-
-//        mapPage.selectAllExceptOneFilter(2);
-//        mapPage.clickAtProblemByCoordinate(50.3, 30.3);
-//
-//        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(2)));
-
-        mapPage.selectOnlyOneFilter(2);
-        mapPage.clickAtProblemByCoordinate(50.3, 30.3);
-
-        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(2)));
-//
-//        mapPage.selectOnlyOneFilter(2);
-//        mapPage.clickAtProblemByCoordinate(50.3, 30.3);
-//
-//        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(2)));
-
-
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.3, 30.3, "ProblemFor Сміттєзвалища", "Сміттєзвалища", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment2");
-//
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.4, 30.4, "ProblemFor Незаконна забудова", "Незаконна забудова", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment3");
-//
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.5, 30.5, "ProblemFor Проблеми водойм", "Проблеми водойм", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment4");
-//
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.6, 30.6, "ProblemFor Загрози біорізноманіттю", "Загрози біорізноманіттю", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment5");
-//
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.7, 30.7, "ProblemFor Браконьєрство", "Браконьєрство", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment6");
-//
-//        new AlertCloserThread().start();
-//        anyPage.addProblem(50.8, 30.8, "ProblemFor Інші проблеми", "Інші проблеми", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment7");
-//
-//        filters.selectAllExceptOneFilter();
-//        filters.selectOnlyOneFilter();
-//
-//        Assert.assertTrue(problemPage.getProblemType().equals(filters.getFilterTitle()));
-
-        //Assert.assertTrue(driver.findElement(By.cssSelector(".form-control.ng-isolate-scope.ng-valid-date")).());
-
-
-        //driver.quit();
+    @Test
+    public void checkFiltersPositive() throws Exception {
+        mapPage.clickZoomOut();
+        mapPage.openFiltersBoard();
+        mapPage.selectAllExceptOneFilter(typeNumber);
+        Assert.assertTrue(problemPage.getProblemType().equals(mapPage.getFilterTitle(typeNumber)));
     }
 }
+//        for dataProvider
+
+//        anyPage.addProblem(50.2, 30.2, "ProblemFor Проблеми лісів", "Проблеми лісів", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment1");
+
+//        anyPage.addProblem(50.3, 30.3, "ProblemFor Сміттєзвалища", "Сміттєзвалища", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment2");
+
+//        anyPage.addProblem(50.4, 30.4, "ProblemFor Незаконна забудова", "Незаконна забудова", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment3");
+//
+//        anyPage.addProblem(50.5, 30.5, "ProblemFor Проблеми водойм", "Проблеми водойм", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment4");
+//
+//        anyPage.addProblem(50.6, 30.6, "ProblemFor Загрози біорізноманіттю", "Загрози біорізноманіттю", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment5");
+//
+//        anyPage.addProblem(50.7, 30.7, "ProblemFor Браконьєрство", "Браконьєрство", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment6");
+//
+//        anyPage.addProblem(50.8, 30.8, "ProblemFor Інші проблеми", "Інші проблеми", "Decsription", "problemProposeTest", "C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg", "comment7");
+//
