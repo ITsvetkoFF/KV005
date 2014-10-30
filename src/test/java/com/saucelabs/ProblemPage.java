bb9ff63652a64b453fcda1d34ba65a737187236a
nickishoff <nickishoff@gmail.com>
2014-10-30 15:01:24 +0200

    editing add(/delete)Comments methods to more universal type. Need little changes.

--- src/test/java/com/saucelabs/ProblemPage.java
+++ src/test/java/com/saucelabs/ProblemPage.java
@@ -2,15 +2,16 @@ package com.saucelabs;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by onikistc on 21.10.2014.
  */
 public class ProblemPage extends AnyPage{
-    public String comment = "New comment";
 
     private WebDriver driver;
     public ProblemPage(WebDriver driver) {
@@ -84,14 +85,57 @@ public class ProblemPage extends AnyPage{
         return comments;
     }
 
-    public void addComment() {
+    public void addComments(double latitude, double longitude, List<String> comments) {
+        clickAtProblemByCoordinate(latitude, longitude);
         driver.findElement(By.xpath("//div[@class='b-problem-tab ng-isolate-scope']/ul/li[2]/a")).click();
-        driver.findElement(By.xpath("//div[@class='b-activity__input-field']/textarea")).sendKeys(comment);
-        driver.findElement(By.xpath("//button[@class='b-activity-comment__btn']")).click();
+        for(String comment : comments) {
+            driver.findElement(By.xpath("//div[@class='b-activity__input-field']/textarea")).sendKeys(comment);
+            driver.findElement(By.xpath("//button[@class='b-activity-comment__btn']")).click();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
     }
 
-    public void deleteComment() {
+    public void deleteComments(double latitude, double longitude) {
+        clickAtProblemByCoordinate(latitude, longitude);
         driver.findElement(By.xpath("//div[@class='b-problem-tab ng-isolate-scope']/ul/li[2]/a")).click();
-        driver.findElement(By.xpath("//div[@class='b-activity__comments']/div[1]/div[2]/span[2]/i")).click();
+
+        List<WebElement> foundActivities = driver.findElements(By.className("b-activity__comments-item"));
+        for(WebElement activity : foundActivities) {
+            if (activity.findElements(By.xpath(".//i[@class='fa fa-weixin b-activity__comments-item-image']")).size() == 1 ) {
+                driver.findElement(By.xpath(".//div[2]/span[2]/i")).click();
+            }
+        }
+    }
+
+    public List<String> getComments() {
+        driver.findElement(By.xpath("//div[@class='b-problem-tab ng-isolate-scope']/ul/li[2]/a")).click();
+        List<String> comments = new ArrayList<>();
+//        int commentsAmount = driver.findElements(By.className("b-activity__comments-item-content")).size() - 1;
+        int commentsAmount = driver.findElements(By.xpath("//div[@class='b-activity__comments-item']/i[@class='fa fa-weixin b-activity__comments-item-image']")).size();
+        for(int i = 1; i <= commentsAmount; i++) {
+            comments.add(0, driver.findElement(By.xpath("//div[@class='b-activity__comments']/div[" + i + "]/div[2]/span[2]")).getAttribute("textContent"));
+        }
+        return comments;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
