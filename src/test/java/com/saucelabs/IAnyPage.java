ff699753595d1d9478c3ed825964fbd6d2cf3a4c
OtyuskiyRoman <otyuskiy@gmail.com>
2014-11-02 14:25:36 +0200

    Added addProblemOffsetPageCenter and clickAtProblemOffsetPageCenter methods (return offset)

--- src/test/java/com/saucelabs/IAnyPage.java
+++ src/test/java/com/saucelabs/IAnyPage.java
@@ -3,11 +3,24 @@ package com.saucelabs;
 import java.util.List;
 
 public interface IAnyPage {
+
     String getFirstResourceTitleFromMenu();
+
     String getFirstResourceTitleFromOpenedResource();
+
     void logIn(String email, String password);
+
     void logOut();
+
     void register(String first_name, String last_name, String email, String password);
+
     String getLoggedInUserName();
-    void addProblem(double latitude, double longitude, String problemName, String problemType, String problemDescription, String problemPropose, List<String> imageUrls, List<String> imageComments);
+
+    void addProblem(double latitude, double longitude, String problemName, String problemType,
+                    String problemDescription, String problemPropose,
+                    List<String> imageUrls, List<String> imageComments);
+
+    int addProblemOffsetPageCenter(double latitude, double longitude, String problemName, String problemType,
+                                   String problemDescription, String problemPropose,
+                                   List<String> imageUrls, List<String> imageComments);
 }
