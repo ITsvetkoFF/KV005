775ba4db6ab15b61b5b135aee0e3c048af05128f
OtyuskiyRoman <otyuskiy@gmail.com>
2014-11-19 21:23:11 +0200

    Sikuli-api integration in progress

--- src/test/java/com/saucelabs/Tests/LocalTests/AddProblemSikuli.java
+++ src/test/java/com/saucelabs/Tests/LocalTests/AddProblemSikuli.java
@@ -1,22 +1,13 @@
 package com.saucelabs.Tests.LocalTests;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.sikuli.api.*;
-import org.sikuli.api.robot.Keyboard;
-import org.sikuli.api.robot.Mouse;
 import org.sikuli.api.robot.desktop.DesktopKeyboard;
-import org.sikuli.api.robot.desktop.DesktopMouse;
-import org.sikuli.api.visual.Canvas;
-import org.sikuli.api.visual.DesktopCanvas;
 import org.testng.annotations.Test;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,51 +17,48 @@ import java.util.concurrent.TimeUnit;
 public class AddProblemSikuli {
 
     @Test
-    public void addProblemSikuli() throws IOException {
+    public void addProblemSikuli() {
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("http://www.ecomap.org/#/map");
-
 
         driver.findElement(By.xpath("//button[@class='navbar-brand b-menu__button']")).click();
         List<WebElement> tabs = driver.findElements(By.xpath("//li[@class='ng-isolate-scope']/a/tab-heading/i"));
         tabs.get(1).click();
         tabs.get(1).click();
 //----------------------------------------------------------------------------------------------------------------------
-        org.openqa.selenium.Dimension dimension = driver.manage().window().getSize();
-        org.openqa.selenium.Dimension additionalDimension = new Dimension(dimension.height, dimension.width - 400);
+//        org.openqa.selenium.Dimension dimension = driver.manage().window().getSize();
+//        org.openqa.selenium.Dimension additionalDimension = new Dimension(dimension.height, dimension.width - 400);
+//
+//        WebDriver additionalDriver = new FirefoxDriver();
+//        additionalDriver.manage().window().setSize(additionalDimension);
+//        additionalDriver.manage().window().setPosition(new Point(0, 0));
+//
+//        additionalDriver.get("http://prntscr.com/57yeoo");
+//
+//        Target dropZone = new ImageTarget(new File(".\\resources\\images\\DropZone.jpg"));
+//        Target koala = new ImageTarget(new File(".\\resources\\images\\KoalaScreen.jpg"));
+//
+//        ScreenRegion dropZoneScreen = new DesktopScreenRegion().wait(dropZone, 20);
+//        ScreenRegion koalaScreen = new DesktopScreenRegion().wait(koala, 20);
+//
+//        Canvas canvas = new DesktopCanvas();
+//        canvas.addLabel(koalaScreen, "We found Koala!!!");
+//        canvas.addLabel(dropZoneScreen, "We found DropZone!!!");
+//        canvas.display(3);
+//
+//        Mouse mouse = new DesktopMouse();
+//        mouse.drag(koalaScreen.getCenter());
+//        mouse.drop(dropZoneScreen.getCenter());
 
-        WebDriver additionalDriver = new FirefoxDriver();
-        additionalDriver.manage().window().setSize(additionalDimension);
-        additionalDriver.manage().window().setPosition(new Point(0, 0));
+        DesktopKeyboard keyboard = new DesktopKeyboard();
+        keyboard.keyDown(KeyEvent.VK_WINDOWS);
+        keyboard.keyDown(KeyEvent.VK_E);
+        keyboard.keyUp(KeyEvent.VK_E);
+        keyboard.keyUp(KeyEvent.VK_WINDOWS);
 
-        additionalDriver.get("http://prntscr.com/57yeoo");
-
-//        additionalDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//        Dimension koalaDimension = additionalDriver.findElement(By.id("screenshot-image")).getSize();
-//        Point koalaPosition = additionalDriver.findElement(By.id("screenshot-image")).getLocation();
-
-        Target dropZone = new ImageTarget(new File(".\\resources\\images\\DropZone.jpg"));
-        Target koala = new ImageTarget(new File(".\\resources\\images\\KoalaScreen.jpg"));
-
-//        BufferedImage bigKoala = ImageIO.read(new URL("http://i.imgur.com/6HuxCqx.jpg"));
-//        BufferedImage smallKoala = bigKoala.getSubimage(koalaPosition.getX(), koalaPosition.getY(),
-//                                                        koalaDimension.getWidth(), koalaDimension.getHeight());
-//        Target koala = new ImageTarget(smallKoala);
-
-        ScreenRegion dropZoneScreen = new DesktopScreenRegion().wait(dropZone, 20);
-        ScreenRegion koalaScreen = new DesktopScreenRegion().wait(koala, 20);
-
-        Canvas canvas = new DesktopCanvas();
-        canvas.addLabel(koalaScreen, "We found Koala!!!");
-        canvas.addLabel(dropZoneScreen, "We found DropZone!!!");
-        canvas.display(3);
-
-        Mouse mouse = new DesktopMouse();
-        mouse.drag(koalaScreen.getCenter());
-        mouse.drop(dropZoneScreen.getCenter());
 //
 //        additionalDriver.navigate().to("http://prntscr.com/57yflf");
 //        ImageTarget desert = new ImageTarget(new URL("http://prntscr.com/57yflf"));
