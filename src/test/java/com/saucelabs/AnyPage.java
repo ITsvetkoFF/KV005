abd701ac45c014dd9ac148150075e59890e7618f
rotiutc <otyuskiy@gmail.com>
2014-11-21 14:09:15 +0200

    Selenium and Sikuli integration in progress

--- src/test/java/com/saucelabs/AnyPage.java
+++ src/test/java/com/saucelabs/AnyPage.java
@@ -18,6 +18,7 @@ import org.sikuli.api.visual.DesktopCanvas;
 import utility.ClipboardUploadThread;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -291,11 +292,17 @@ public class AnyPage extends MapPage implements IAnyPage {
         BufferedImage bigKoala = ImageIO.read(new File("C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg"));
         BufferedImage smallKoala = bigKoala.getSubimage(320, 120, 600, 600);
 
-        Target koala = new ImageTarget(new File(".\\resources\\images\\Koala.jpg"));
-        koala.setMinScore(0.8);
+        BufferedImage resizeBigKoala = resize(bigKoala, 85, 64);
+        Target resizeBigKoalaTarget = new ImageTarget(resizeBigKoala);
+        resizeBigKoalaTarget.setMinScore(0.7);
 
-        Target desert = new ImageTarget(new File(".\\resources\\images\\Desert.jpg"));
-        desert.setMinScore(0.8);
+        BufferedImage bigDesert = ImageIO.read(new File("C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg"));
+        BufferedImage resizeBigDesert = resize(bigDesert, 85, 64);
+        Target resizeBigDesertTarget = new ImageTarget(resizeBigDesert);
+        resizeBigDesertTarget.setMinScore(0.7);
+
+        Target cross = new ImageTarget(new File(".\\resources\\images\\Cross.jpg"));
+        cross.setMinScore(0.8);
 
         keyboard.keyDown(KeyEvent.VK_WINDOWS);
         keyboard.keyDown(KeyEvent.VK_E);
@@ -328,11 +335,26 @@ public class AnyPage extends MapPage implements IAnyPage {
         }
         canvas.display(1);
         canvas.clear();
-        canvas.addBox(screenRegionList.get(1));
-        canvas.addLabel(screenRegionList.get(1), "We get this!");
+
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+        for (ScreenRegion screen : screenRegionList) {
+            if (screen.getCenter().getX() > x) {
+                x = screen.getCenter().getX();
+                y = screen.getCenter().getY();
+                width = screen.getBounds().width;
+                height = screen.getBounds().height;
+            }
+        }
+        screenRegion = new DesktopScreenRegion(x, y, width, height);
+
+        canvas.addCircle(screenRegion.getCenter());
+        canvas.addLabel(screenRegion.getCenter(), "We get this!");
         canvas.display(1);
         canvas.clear();
-        mouse.click(screenRegionList.get(1).getRelativeScreenLocation(100, 0));
+        mouse.click(screenRegion.getCenter().getRelativeScreenLocation(100, 0));
         keyboard.paste("C:\\Users\\Public\\Pictures\\Sample Pictures");
         keyboard.keyDown(KeyEvent.VK_ENTER);
         keyboard.keyUp(KeyEvent.VK_ENTER);
@@ -343,18 +365,21 @@ public class AnyPage extends MapPage implements IAnyPage {
         canvas.display(1);
         canvas.clear();
 
-        canvas.addImage(screenRegionList.get(1).getCenter(), smallKoala);
-        canvas.addLabel(screenRegionList.get(1), "           We want choose this nice Koala!");
-        canvas.display(3);
+        canvas.addImage(screenRegionList.get(0).getCenter(), smallKoala);
+        canvas.addLabel(screenRegionList.get(0), "           We want choose this nice Koala!");
+        canvas.display(1);
         canvas.clear();
 
-        ScreenRegion koalaScreenRegion = new DesktopScreenRegion().wait(koala, 10);
+        ScreenRegion koalaScreenRegion = new DesktopScreenRegion().wait(resizeBigKoalaTarget, 10);
         mouse.drag(koalaScreenRegion.getCenter());
         mouse.drop(dropZoneScreenRegion.getCenter());
 
-        ScreenRegion desertScreenRegion = new DesktopScreenRegion().wait(desert, 10);
+        ScreenRegion desertScreenRegion = new DesktopScreenRegion().wait(resizeBigDesertTarget, 10);
         mouse.drag(desertScreenRegion.getCenter());
         mouse.drop(dropZoneScreenRegion.getCenter());
+
+        ScreenRegion crossScreenRegion = new DesktopScreenRegion().wait(cross, 10);
+        mouse.click(crossScreenRegion.getCenter());
 
         /*------------------------------------------------------------------------------------------------------------*/
 
@@ -376,5 +401,17 @@ public class AnyPage extends MapPage implements IAnyPage {
         } catch (Exception e) {
         }
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    private BufferedImage resize(BufferedImage img, int newW, int newH) {
+        int w = img.getWidth();
+        int h = img.getHeight();
+        BufferedImage dImg = new BufferedImage(newW, newH, img.getType());
+        Graphics2D g = dImg.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null);
+        g.dispose();
+        return dImg;
     }
 }
